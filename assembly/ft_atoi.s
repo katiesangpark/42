ed52bee1b289259b0c1dcccb617daed71da9f64b
Killian CAUSSE <kicausse@e3r2p6.42.fr>
2018-12-21 18:09:25 +0100

    add atoi

---
+++ assembly/ft_atoi.s
@@ -0,0 +1,32 @@
+.intel_syntax noprefix
+.globl _ft_atoi
+_ft_atoi:
+	cmp		BYTE ptr[rdi], 32
+	jne		start
+	inc		rdi
+	jmp		_ft_atoi
+start:
+	xor 	rax, rax
+	xor		sil, sil
+	cmp		BYTE ptr[rdi], 45
+	jne 	mainloop
+	mov 	sil, 1
+	inc		rdi
+mainloop:
+	movsx 	rcx, BYTE ptr[rdi]
+	cmp 	rcx, 48
+	jl		end
+	cmp		rcx, 57
+	jg		end
+	sub		rcx, 48
+	imul	rax, 10
+	inc 	rdi
+	test	sil, sil
+	jnz		neg
+	add 	rax, rcx
+	jmp 	mainloop
+neg:
+	sub 	rax, rcx
+	jmp 	mainloop
+end:
+	ret
