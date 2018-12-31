c775934b89d1d95d7ef1ea769aaf6036cff513e2
kilo <kilowwwww@gmail.com>
2018-12-31 14:19:30 +0100

    Create sort_list.c

---
+++ StudExams/sort_list/sort_list.c
@@ -0,0 +1,29 @@
+#include "list.h"
+
+t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
+{
+	(void)cmp;
+	asm(".intel_syntax;\
+			mov 	r8, rdi;\
+			mov 	r9, rsi;\
+			mov 	r10, r8;\
+			test	rdi, -1;\
+			jz 		end;
+			MAINLOOP: mov r15, QWORD ptr[r10 + 8];\
+			test 	r15, -1;\
+			jz 		end;\
+			mov		edi, DWORD ptr[r10];\
+			mov		esi, DWORD ptr[r15];\
+			call	r9;\
+			jnz		NEXTELEM;\
+			mov		edi, DWORD ptr[r10];\
+			mov		esi, DWORD ptr[r15];\
+			mov		DWORD ptr[r10], esi;\
+			mov		DWORD ptr[r15], edi;\
+			mov		r10, r8;\
+			jmp		MAINLOOP;\
+			NEXTELEM: mov		r10, r15;\
+			jmp 	MAINLOOP;\
+			end: mov rax, r8;");
+	return (lst);
+}
