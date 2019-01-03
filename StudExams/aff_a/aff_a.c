54d8f02f9d7d6a5b6ca2066cf124ef624f13312a
kilo <kilowwwww@gmail.com>
2019-01-03 21:39:30 +0100

    Update aff_a.c

--- StudExams/aff_a/aff_a.c
+++ StudExams/aff_a/aff_a.c
@@ -1,8 +1,5 @@
-
-int main(int argc, char **argv)
+__attribute__ ((naked))
+int main(void)
 {
-	(void)argc;
-	(void)argv;
-	asm(".intel_syntax; cmp rdi, 1; je newline; mov rsi, QWORD ptr[rsi + 8]; mainloop: mov cl, BYTE ptr[rsi]; cmp cl, 'a'; je print; test cl, -1; jz newline; inc rsi;  jmp mainloop; print: push 'a'; mov rax, 0x2000004; mov rdi, 1; lea rsi, [rsp]; mov rdx, 1;SYSCALL; pop rax; newline: push 0xa; mov rax, 0x2000004; mov rdi, 1; lea rsi, [rsp]; mov rdx, 1;SYSCALL; pop rax;");
-	return (0);
+	asm(".intel_syntax; cmp rdi, 1; je newline; mov rsi, QWORD ptr[rsi + 8]; mainloop: mov cl, BYTE ptr[rsi]; cmp cl, 'a'; je print; test cl, -1; jz newline; inc rsi;  jmp mainloop; print: push 'a'; mov rax, 0x2000004; mov rdi, 1; lea rsi, [rsp]; mov rdx, 1;SYSCALL; pop rax; newline: push 0xa; mov rax, 0x2000004; mov rdi, 1; lea rsi, [rsp]; mov rdx, 1;SYSCALL; pop rax; xor eax, eax; ret;");
 }
