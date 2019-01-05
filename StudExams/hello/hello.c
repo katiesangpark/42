83aa5fda92844e8d016aa1f0fff02e86c05ef3ff
kilo <kilowwwww@gmail.com>
2019-01-05 08:41:21 +0100

    Update hello.c

--- StudExams/hello/hello.c
+++ StudExams/hello/hello.c
@@ -1,9 +1,15 @@
-
-int main(int ac, char **av)
+__attribute__ ((naked))
+int main(void)
 {
-	(void)ac;
-	(void)av;
-	(void)(char*){"Hello World!\n"};
-	asm(".intel_syntax; mov rax, 0x2000004; mov rdi, 1; lea rsi, [rip + L_.str]; mov rdx, 13; SYSCALL;");
-	return (0);
+	asm(".intel_syntax;\
+			mov rax, 0x2000004;\
+			mov rdi, 1;\
+			lea rsi, [rip + message];\
+			mov rdx, 13;\
+			SYSCALL;\
+			xor eax, eax;\
+			ret;\
+			\
+			message:\
+				.asciz \"Hello world!\n\";");	
 }
