9dbe6fe7d3b8af0b7287b3ec3d06afb91fb88cb5
Killian CAUSSE <kicausse@e2r11p20.42.fr>
2019-01-24 06:01:53 +0100

    Further progress on parsing, especially on norming

--- minishell/srcs/parser.c
+++ minishell/srcs/parser.c
@@ -11,21 +11,49 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "parsing.h"
 
 int		count_arguments(char *input)
 {
 	unsigned int	count;
 	unsigned int	quote;
-	unsigned int	s;
 	unsigned int	e;
 
-	s = 0;
 	count = 0;
-	while (++count && input[s] && !(quote = 0))
+	while (++count && *input && !(quote = 0))
 	{
-		while (input[s] == '\t' || input[s] == ' ')
-			++s;
-		e = s;
+		ignore_chars(&input, "\t ");
+		if (*input == '\0' || (e = 0))
+			break ;
+		e = 0;
+		while (input[e] && input[e] != '\t' && input[e] != ' ')
+		{
+			if (input[e] == '"' && (quote = 1) && ++e)
+			{
+				while (input[e] != '"' && input[e] != '\0')
+					++e;
+				break ;
+			}
+			else
+				++e;
+		}
+		input += *(input + e) == '\0' ? e : e + 1 + quote;
+	}
+	return (count);
+}
+
+void	fill_args(char **args, char *input)
+{
+	unsigned int	count;
+	unsigned int	quote;
+	unsigned int	e;
+
+	count = 0;
+	while (*input && !(quote = 0))
+	{
+		ignore_chars(&input, "\t ");
+		if (*input == '\0' || (e = 0))
+			break ;
 		while (input[e] != '\t' && input[e] != ' ' && input[e] != '\0')
 		{
 			if (input[e] == '"' && (quote = 1) && ++e)
@@ -37,45 +65,18 @@ int		count_arguments(char *input)
 			else
 				++e;
 		}
-		s = input[e] == '\0' ? e : e + 1 + quote;
+		args[count++] = ft_strsub(input, 0, e - quote);
+		input += *(input + e) == '\0' ? e : e + 1 + quote;
 	}
-	return (count);
+	args[count] = NULL;
 }
 
 char	**parse_input(char *input)
 {
 	char			**args;
-	unsigned int	count;
-	unsigned int	quote;
-	unsigned int	s;
-	unsigned int	e;
 
 	if (!(args = ft_memalloc(sizeof(char*) * count_arguments(input))))
 		return (NULL);
-	s = 0;
-	count = 0;
-	while (input[s])
-	{
-		quote = 0;
-		while (input[s] == '\t' || input[s] == ' ')
-			++s;
-		e = s;
-		while (input[e] != '\t' && input[e] != ' ' && input[e] != '\0')
-		{
-			if (input[e] == '"')
-			{
-				quote = 1;
-				++e;
-				while (input[e] != '"' && input[e] != '\0')
-					++e;
-				break ;
-			}
-			else
-				++e;
-		}
-		args[count++] = ft_strsub(input, s + quote, e - s - quote);
-		s = input[e] == '\0' ? e : e + 1 + quote;
-	}
-	args[count] = NULL;
+	fill_args(args, input);
 	return (args);
 }
--- minishell/srcs/parser_utils.c
+++ minishell/srcs/parser_utils.c
@@ -1,23 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing.h                                          :+:      :+:    :+:   */
+/*   parser_utils.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kicausse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/23 21:15:43 by kicausse          #+#    #+#             */
-/*   Updated: 2019/01/23 21:15:43 by kicausse         ###   ########.fr       */
+/*   Created: 2019/01/24 05:40:29 by kicausse          #+#    #+#             */
+/*   Updated: 2019/01/24 05:40:29 by kicausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSING_H
-# define PARSING_H
+#include "libft.h"
 
-void	read_input(char *buf);
+void	ignore_chars(char **dest, char *charset)
+{
+	while (**dest && ft_cfind(charset, **dest) >= 0)
+		++(*dest);
+}
 
-void	ignore_chars(char **dest, char *charset);
-int		look_for_chars(char *dest, char *charset);
+int		look_for_chars(char *dest, char *charset)
+{
+	unsigned int	i;
 
-char	**parse_input(char *input);
-
-#endif
+	i = 0;
+	while (dest[i] && ft_cfind(charset, dest[i]) == -1)
+		++i;
+	return (i);
+}
